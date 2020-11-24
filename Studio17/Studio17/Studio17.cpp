c28f41015b1dc58868ae6b1bb88280425e4cba81
ziwen0310 <54512760+ziwen0310@users.noreply.github.com>
2020-11-24 14:18:43 +0800

    studio17 readme

--- Studio17/Studio17/Studio17.cpp
+++ Studio17/Studio17/Studio17.cpp
@@ -2,10 +2,18 @@
 //
 
 using namespace std;
-
+#include "..\..\\SharedCode\ImageFile.h"
 
 int main()
 {
+	auto a = ImageFile("asd");
+	std::vector<char> tem;
+	for (int i = 0; i < 9; ++i) {
+		tem.push_back('X');
+	}
+	tem.push_back('3');
+	a.write(tem);
+	a.read();
 	return 0;
 }
 
