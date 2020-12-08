e16af7c728f35c3b31be12f41b8e7f2291e9b0e4
ziwen0310 <54512760+ziwen0310@users.noreply.github.com>
2020-12-08 12:24:47 +0800

    s20 readme

--- Studio20/Studio20/ReadMe.txt
+++ Studio20/Studio20/ReadMe.txt
@@ -1,1 +1,9 @@
 Studio 20 ReadMe.txt
+1. Ziwen Wang, Yanpeng Yuan, Sally Lee
+2. Since the PasswordProxy object is the only handle to a file, we need to delete the file object it's pointing to when we destruct the PasswordProxt object, making sure it will not be used anymore.
+4. We set the password to be "123". We first call write() and read() to write some contents and print it out. In the terminal we are asked to enter the password twice, and then output:X   X   X   X   X, as expected.
+Then we try the accept() to display. After entering the password twice, we have this output on the terminal:
+X X
+ X
+X X
+as expected.
--- Studio20/Studio20/Studio20.cpp
+++ Studio20/Studio20/Studio20.cpp
@@ -1,8 +1,31 @@
 // Studio20.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+#include <iostream>
+#include <string>
+#include <cstddef>
+#include "..\..\\SharedCode\AbstractFileSystem.h"
+#include "..\..\\SharedCode\SimpleFileFactory.h"
+#include "..\..\\SharedCode\SimpleFileSystem.h"
+#include "..\..\\SharedCode\AbstractFile.h"
+#include "..\..\\SharedCode\PasswordProxy.h"
+#include "..\..\\SharedCode\BasicDisplayVisitor.h"
 int main()
 {
-	return 0;
+	AbstractFileSystem* absSys = new SimpleFileSystem();
+
+	AbstractFileFactory* absFactory = new SimpleFileFactory();
+
+	AbstractFile* txt = absFactory->createFile("1.img");
+	PasswordProxy tem(txt, "123");
+	std::vector<char> input = { 'X', ' ', 'X', ' ', 'X', ' ', 'X',' ', 'X', '3' };
+	tem.write(input);
+	/*vector<char>output = tem.read();
+
+	for (std::vector<char>::const_iterator i = output.begin(); i != output.end(); ++i) {
+		std::cout << *i << ' ';
+	}*/
+	BasicDisplayVisitor* visitor = new BasicDisplayVisitor;
+
+	tem.accept(visitor);
 }
 
